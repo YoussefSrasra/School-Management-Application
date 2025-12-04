a892e7ad5255b43dcb6935ac33c7044043c57753
YoussefSrasra <youssefsrasra8@gmail.com>
2025-12-04 23:18:12 +0100

    feat: implement auth guard

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { StudentListComponent } from './components/student-list/student-list.component';
+import {authGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'students', component: StudentListComponent },
+  { path: 'students', component: StudentListComponent , canActivate: [authGuard]},
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
