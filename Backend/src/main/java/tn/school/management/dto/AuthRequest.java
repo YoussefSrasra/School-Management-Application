82ac77664ae31ac066ed4358345f9aaef3c7a854
YoussefSrasra <youssefsrasra8@gmail.com>
2025-12-04 09:43:02 +0100

    feat: implement DTOs with validation constraints

---
+++ Backend/src/main/java/tn/school/management/dto/AuthRequest.java
@@ -0,0 +1,22 @@
+package tn.school.management.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+}
---
+++ Backend/src/main/java/tn/school/management/dto/StudentDto.java
@@ -0,0 +1,26 @@
+package tn.school.management.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tn.school.management.entity.Level;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDto {
+
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotNull(message = "Level is required")
+    private Level level;
+}
